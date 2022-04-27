ba2931dc906e479112569fd45223aa972c5fb40d
Lukas Benes <bender@false.cz>
2022-04-27 16:27:25 +0200

    bump

---
+++ InstaCover.xcframework/ios-arm64_x86_64-simulator/InstaCover.framework/Headers/InstaCover.h
@@ -0,0 +1,225 @@
+#import <Foundation/NSArray.h>
+#import <Foundation/NSDictionary.h>
+#import <Foundation/NSError.h>
+#import <Foundation/NSObject.h>
+#import <Foundation/NSSet.h>
+#import <Foundation/NSString.h>
+#import <Foundation/NSValue.h>
+
+@class InstaCoverHelpersCompanion, UIView, NSObject, InstaCoverLogPayload;
+
+@protocol InstaCoverInstaCarListener;
+
+NS_ASSUME_NONNULL_BEGIN
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunknown-warning-option"
+#pragma clang diagnostic ignored "-Wincompatible-property-type"
+#pragma clang diagnostic ignored "-Wnullability"
+
+#pragma push_macro("_Nullable_result")
+#if !__has_feature(nullability_nullable_result)
+#undef _Nullable_result
+#define _Nullable_result _Nullable
+#endif
+
+__attribute__((swift_name("KotlinBase")))
+@interface InstaCoverBase : NSObject
+- (instancetype)init __attribute__((unavailable));
++ (instancetype)new __attribute__((unavailable));
++ (void)initialize __attribute__((objc_requires_super));
+@end;
+
+@interface InstaCoverBase (InstaCoverBaseCopying) <NSCopying>
+@end;
+
+__attribute__((swift_name("KotlinMutableSet")))
+@interface InstaCoverMutableSet<ObjectType> : NSMutableSet<ObjectType>
+@end;
+
+__attribute__((swift_name("KotlinMutableDictionary")))
+@interface InstaCoverMutableDictionary<KeyType, ObjectType> : NSMutableDictionary<KeyType, ObjectType>
+@end;
+
+@interface NSError (NSErrorInstaCoverKotlinException)
+@property (readonly) id _Nullable kotlinException;
+@end;
+
+__attribute__((swift_name("KotlinNumber")))
+@interface InstaCoverNumber : NSNumber
+- (instancetype)initWithChar:(char)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedChar:(unsigned char)value __attribute__((unavailable));
+- (instancetype)initWithShort:(short)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedShort:(unsigned short)value __attribute__((unavailable));
+- (instancetype)initWithInt:(int)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedInt:(unsigned int)value __attribute__((unavailable));
+- (instancetype)initWithLong:(long)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedLong:(unsigned long)value __attribute__((unavailable));
+- (instancetype)initWithLongLong:(long long)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedLongLong:(unsigned long long)value __attribute__((unavailable));
+- (instancetype)initWithFloat:(float)value __attribute__((unavailable));
+- (instancetype)initWithDouble:(double)value __attribute__((unavailable));
+- (instancetype)initWithBool:(BOOL)value __attribute__((unavailable));
+- (instancetype)initWithInteger:(NSInteger)value __attribute__((unavailable));
+- (instancetype)initWithUnsignedInteger:(NSUInteger)value __attribute__((unavailable));
++ (instancetype)numberWithChar:(char)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedChar:(unsigned char)value __attribute__((unavailable));
++ (instancetype)numberWithShort:(short)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedShort:(unsigned short)value __attribute__((unavailable));
++ (instancetype)numberWithInt:(int)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedInt:(unsigned int)value __attribute__((unavailable));
++ (instancetype)numberWithLong:(long)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedLong:(unsigned long)value __attribute__((unavailable));
++ (instancetype)numberWithLongLong:(long long)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedLongLong:(unsigned long long)value __attribute__((unavailable));
++ (instancetype)numberWithFloat:(float)value __attribute__((unavailable));
++ (instancetype)numberWithDouble:(double)value __attribute__((unavailable));
++ (instancetype)numberWithBool:(BOOL)value __attribute__((unavailable));
++ (instancetype)numberWithInteger:(NSInteger)value __attribute__((unavailable));
++ (instancetype)numberWithUnsignedInteger:(NSUInteger)value __attribute__((unavailable));
+@end;
+
+__attribute__((swift_name("KotlinByte")))
+@interface InstaCoverByte : InstaCoverNumber
+- (instancetype)initWithChar:(char)value;
++ (instancetype)numberWithChar:(char)value;
+@end;
+
+__attribute__((swift_name("KotlinUByte")))
+@interface InstaCoverUByte : InstaCoverNumber
+- (instancetype)initWithUnsignedChar:(unsigned char)value;
++ (instancetype)numberWithUnsignedChar:(unsigned char)value;
+@end;
+
+__attribute__((swift_name("KotlinShort")))
+@interface InstaCoverShort : InstaCoverNumber
+- (instancetype)initWithShort:(short)value;
++ (instancetype)numberWithShort:(short)value;
+@end;
+
+__attribute__((swift_name("KotlinUShort")))
+@interface InstaCoverUShort : InstaCoverNumber
+- (instancetype)initWithUnsignedShort:(unsigned short)value;
++ (instancetype)numberWithUnsignedShort:(unsigned short)value;
+@end;
+
+__attribute__((swift_name("KotlinInt")))
+@interface InstaCoverInt : InstaCoverNumber
+- (instancetype)initWithInt:(int)value;
++ (instancetype)numberWithInt:(int)value;
+@end;
+
+__attribute__((swift_name("KotlinUInt")))
+@interface InstaCoverUInt : InstaCoverNumber
+- (instancetype)initWithUnsignedInt:(unsigned int)value;
++ (instancetype)numberWithUnsignedInt:(unsigned int)value;
+@end;
+
+__attribute__((swift_name("KotlinLong")))
+@interface InstaCoverLong : InstaCoverNumber
+- (instancetype)initWithLongLong:(long long)value;
++ (instancetype)numberWithLongLong:(long long)value;
+@end;
+
+__attribute__((swift_name("KotlinULong")))
+@interface InstaCoverULong : InstaCoverNumber
+- (instancetype)initWithUnsignedLongLong:(unsigned long long)value;
++ (instancetype)numberWithUnsignedLongLong:(unsigned long long)value;
+@end;
+
+__attribute__((swift_name("KotlinFloat")))
+@interface InstaCoverFloat : InstaCoverNumber
+- (instancetype)initWithFloat:(float)value;
++ (instancetype)numberWithFloat:(float)value;
+@end;
+
+__attribute__((swift_name("KotlinDouble")))
+@interface InstaCoverDouble : InstaCoverNumber
+- (instancetype)initWithDouble:(double)value;
++ (instancetype)numberWithDouble:(double)value;
+@end;
+
+__attribute__((swift_name("KotlinBoolean")))
+@interface InstaCoverBoolean : InstaCoverNumber
+- (instancetype)initWithBool:(BOOL)value;
++ (instancetype)numberWithBool:(BOOL)value;
+@end;
+
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("Helpers")))
+@interface InstaCoverHelpers : InstaCoverBase
+- (instancetype)init __attribute__((swift_name("init()"))) __attribute__((objc_designated_initializer));
++ (instancetype)new __attribute__((availability(swift, unavailable, message="use object initializers instead")));
+@property (class, readonly, getter=companion) InstaCoverHelpersCompanion *companion __attribute__((swift_name("companion")));
+@end;
+
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("Helpers.Companion")))
+@interface InstaCoverHelpersCompanion : InstaCoverBase
++ (instancetype)alloc __attribute__((unavailable));
++ (instancetype)allocWithZone:(struct _NSZone *)zone __attribute__((unavailable));
++ (instancetype)companion __attribute__((swift_name("init()")));
+@property (class, readonly, getter=shared) InstaCoverHelpersCompanion *shared __attribute__((swift_name("shared")));
+- (NSString *)buildUrlSessionId:(NSString *)sessionId __attribute__((swift_name("buildUrl(sessionId:)")));
+- (void)logToBackendSessionId:(NSString *)sessionId message:(NSString *)message __attribute__((swift_name("logToBackend(sessionId:message:)")));
+@end;
+
+__attribute__((swift_name("InstaCarListener")))
+@protocol InstaCoverInstaCarListener
+@required
+- (void)onError __attribute__((swift_name("onError()")));
+- (void)onFinished __attribute__((swift_name("onFinished()")));
+- (void)onLoaded __attribute__((swift_name("onLoaded()")));
+- (void)onLoading __attribute__((swift_name("onLoading()")));
+@end;
+
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("InstaCover")))
+@interface InstaCoverInstaCover : InstaCoverBase
+- (instancetype)init __attribute__((swift_name("init()"))) __attribute__((objc_designated_initializer));
++ (instancetype)new __attribute__((availability(swift, unavailable, message="use object initializers instead")));
+- (UIView *)createInstacarViewSessionId:(NSString *)sessionId stateChangeListener:(id<InstaCoverInstaCarListener> _Nullable)stateChangeListener __attribute__((swift_name("createInstacarView(sessionId:stateChangeListener:)")));
+@property (readonly) NSObject *ic __attribute__((swift_name("ic")));
+@end;
+
+__attribute__((unavailable("Kotlin subclass of Objective-C class can't be imported")))
+__attribute__((swift_name("InstacarIOS")))
+@interface InstaCoverInstacarIOS : NSObject
+@end;
+
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("LogPayload")))
+@interface InstaCoverLogPayload : InstaCoverBase
+- (instancetype)initWithMessage:(NSString *)message __attribute__((swift_name("init(message:)"))) __attribute__((objc_designated_initializer));
+- (NSString *)component1 __attribute__((swift_name("component1()")));
+- (InstaCoverLogPayload *)doCopyMessage:(NSString *)message __attribute__((swift_name("doCopy(message:)")));
+- (BOOL)isEqual:(id _Nullable)other __attribute__((swift_name("isEqual(_:)")));
+- (NSUInteger)hash __attribute__((swift_name("hash()")));
+- (NSString *)description __attribute__((swift_name("description()")));
+@property (readonly) NSString *message __attribute__((swift_name("message")));
+@end;
+
+__attribute__((unavailable("Kotlin subclass of Objective-C class can't be imported")))
+__attribute__((swift_name("MessageHandlerIOS")))
+@interface InstaCoverMessageHandlerIOS : NSObject
+@end;
+
+__attribute__((objc_subclassing_restricted))
+__attribute__((swift_name("MessageHandlerMapper")))
+@interface InstaCoverMessageHandlerMapper : InstaCoverBase
+- (instancetype)initWithListener:(id<InstaCoverInstaCarListener> _Nullable)listener __attribute__((swift_name("init(listener:)"))) __attribute__((objc_designated_initializer));
+- (void)handleMessageMessage:(NSString *)message __attribute__((swift_name("handleMessage(message:)")));
+@end;
+
+__attribute__((unavailable("Kotlin subclass of Objective-C class can't be imported")))
+__attribute__((swift_name("NavigatorGeolocation")))
+@interface InstaCoverNavigatorGeolocation : NSObject
+@end;
+
+__attribute__((unavailable("Kotlin subclass of Objective-C class can't be imported")))
+__attribute__((swift_name("WebViewSessionWrapper")))
+@interface InstaCoverWebViewSessionWrapper : NSObject
+@end;
+
+#pragma pop_macro("_Nullable_result")
+#pragma clang diagnostic pop
+NS_ASSUME_NONNULL_END
